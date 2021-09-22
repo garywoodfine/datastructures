053237faa3e4c3ec03ad8e9a764ba6c8f9d30e78
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-09-22 17:29:45 +0100

    added sample code

---
+++ src/queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            // Adds elements {0, 1, 2, 3, 4} to queue
+            for (int i = 0; i < 5; i++)
+                queue.Enqueue(i);
+
+            // Print out Elements of the Queue
+            Console.Write("Elements of Queue: ");
+            queue.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+            
+            // To remove the head of queue.
+            // In this the oldest element '0' will be removed
+            Console.WriteLine("Going to Dequeue element");
+
+         
+            Console.WriteLine($"removed element {queue.Dequeue().ToString()}");
+
+            Console.Write("Elements of Queue: ");
+            queue.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("View the head of queue");
+           Console.WriteLine($"head of queue- {queue.Peek().ToString()}");
+            Console.WriteLine(Environment.NewLine);
+            
+           
+            Console.WriteLine($"Size of queue- {queue.Count}");
+            
+            //Add another item to queue
+            Console.WriteLine("Add new element to queue");
+            queue.Enqueue(5);
+            
+            Console.Write("Elements of Queue: ");
+            queue.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
---
+++ src/stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create and Add items to a stack
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < 5; i++)
+                stack.Push(i);
+            
+            Console.Write("Elements of stack: ");
+            stack.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+            
+            Console.WriteLine("Going to Pop element off stack");
+
+         
+            Console.WriteLine($"removed element {stack.Pop().ToString()}");
+            
+            Console.Write("Elements of Stack: ");
+            stack.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+            
+            Console.WriteLine("View the head of stack");
+            Console.WriteLine($"head of stack- {stack.Peek().ToString()}");
+            Console.WriteLine(Environment.NewLine);
+            
+           
+            Console.WriteLine($"Size of stack- {stack.Count}");
+            
+            Console.WriteLine($"Add element to stack");
+            stack.Push(5);
+            Console.Write("Elements of Stack: ");
+            stack.ToList().ForEach(x => Console.Write($"{x.ToString()} "));
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
